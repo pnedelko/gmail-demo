5a48137611e18789d409a3dcc212476fd03435f5
Pavel Nedelko <pnedelko@gmail.com>
2017-07-18 13:17:52 +0200

    added eslint

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -1,8 +1,22 @@
 'use strict';
 
 const google = require('googleapis');
+const gmail = google.gmail('v1');
+const oauth2Client = new google.auth.OAuth2();
+const { promisify } = require('util');
 
 module.exports = async function verify(credentials) {
     console.log('Credentials %j', credentials);
-    throw new Error('Invalid creds');
+
+    oauth2Client.setCredentials(credentials.oauth);
+
+    const getProfile = promisify(gmail.users.getProfile);
+    const result = await getProfile({
+        userId: 'me',
+        auth: oauth2Client
+    });
+
+    console.log('Profile: %j', result);
+
+    return result;
 };
